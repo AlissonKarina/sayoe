ee2fc64a7578f287c3e52f31013ca8df11b47f45
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-02-28 21:19:37 -0500

    cambio en DB cursos observados

--- database/migrations/2019_02_26_150051_create_cursos_observados_table.php
+++ database/migrations/2019_02_26_150051_create_cursos_observados_table.php
@@ -18,9 +18,8 @@ class CreateCursosObservadosTable extends Migration
             $table->string('nombre');
             $table->integer('nota');
             $table->integer('id_alumno')->unsigned();
-            $table->string('codigo_alumno', 8);
-
             $table->foreign('id_alumno')->references('id')->on('alumnos');
+            $table->string('codigo_alumno', 8);
             $table->foreign('codigo_alumno')->references('codigo')->on('alumnos');
         });
     }
