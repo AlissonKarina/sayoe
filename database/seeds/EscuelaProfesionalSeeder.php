85b47cb5497cc502e586fb41e15be6fadb9afe54
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-02-28 21:55:56 -0500

    faker 2

--- database/seeds/EscuelaProfesionalSeeder.php
+++ database/seeds/EscuelaProfesionalSeeder.php
@@ -12,11 +12,11 @@ class EscuelaProfesionalSeeder extends Seeder
      */
     public function run()
     {
-        factory(EscuelaProfesional::class)->times(1)->create([
+        EscuelaProfesional::create([
             'nombre' => 'INGENIERÍA DE SISTEMAS',
             'id_facultad' => '1'
         ]);
-        factory(EscuelaProfesional::class)->times(1)->create([
+        EscuelaProfesional::create([
             'nombre' => 'INGENIERÍA DE SOFTWARE',
             'id_facultad' => '1'
         ]);
